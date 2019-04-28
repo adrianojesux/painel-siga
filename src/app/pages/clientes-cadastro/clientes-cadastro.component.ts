ae1c66b05b3e13ea67bb2f3dbb289f12ce4c78bf
Adriano Silva <asjesus1994@gmail.com>
2019-04-28 03:37:39 -0300

    finalização do estilo na página de cadastro de cliente

--- src/app/pages/clientes-cadastro/clientes-cadastro.component.ts
+++ src/app/pages/clientes-cadastro/clientes-cadastro.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Cliente from 'src/app/models/clients';
 
 @Component({
   selector: 'app-clientes-cadastro',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClientesCadastroComponent implements OnInit {
 
+  blockActive: number = 0;
+
   constructor() { }
 
+  cliente: Cliente = new Cliente();
+
   ngOnInit() {
+  }
+
+  doSubmit(form){
+    console.log(form)
+  }
+
+  activateBlock(b: number){
+    this.blockActive = b;
   }
 
 }
