f00466512118df4450186e330adde4f7ef9be727
Adriano Silva <asjesus1994@gmail.com>
2019-04-28 03:36:21 -0300

    criação da função para gerar senha forte

---
+++ src/app/utils/functions-utils.ts
@@ -0,0 +1,16 @@
+export class Functions {
+    static generatePassword(len: number) {
+        var pwd = [],
+            cc = String.fromCharCode,
+            R = Math.random,
+            rnd, i;
+        pwd.push(cc(48 + (0 | R() * 10))); // push a number
+        pwd.push(cc(65 + (0 | R() * 26))); // push an upper case letter
+        for (i = 2; i < len; i++) {
+            rnd = 0 | R() * 62; // generate upper OR lower OR number
+            pwd.push(cc(48 + rnd + (rnd > 9 ? 7 : 0) + (rnd > 35 ? 6 : 0)));
+        }
+        // shuffle letters in password
+        return pwd.sort(function() { return R() - .5; }).join('');
+    }
+}
